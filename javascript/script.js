a1d6533dc26c08494ca1799d8e801dc79452fb67
Dprof-Code <olawaleade15@gmail.com>
2023-02-26 22:56:10 -0600

    completed mobile view

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 const menuBtn = document.getElementById("menu-icon");
 
-const menu = document.getElementById("nav");
+const menu = document.getElementById("nav-menu");
 
 menuBtn.addEventListener('click', function menuEvent() {
     menu.classList.toggle('active');
